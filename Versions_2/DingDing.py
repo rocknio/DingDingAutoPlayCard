063a129412063231583e2566e29b339a4e8ef1ec
rocknio <rocknio@gmail.com>
2018-10-26 18:20:26 +0800

    单次打卡ok

--- Versions_2/DingDing.py
+++ Versions_2/DingDing.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 import subprocess
 import time
-import sched
+from tornado import ioloop
 import datetime
 import random
 import configparser
 
 config = configparser.ConfigParser(allow_no_value=False)
 config.read("dingding.cfg")
-scheduler = sched.scheduler(time.time, time.sleep)
 go_hour = int(config.get("time", "go_hour"))
 back_hour = int(config.get("time", "back_hour"))
 directory = config.get("ADB", "directory")
-is_debug = config.get("GLOBAL", "is_debug")
+is_debug = int(config.get("GLOBAL", "is_debug"))
 
 
 # 打开钉钉，关闭钉钉封装为一个妆饰器函数
@@ -34,8 +33,10 @@ def with_open_close_dingding(func):
             time.sleep(2)
         time.sleep(15)
         print("打开企业考勤界面成功")
+
         # 包装函数
         func(self, *args, **kwargs)
+
         print("关闭钉钉")
         operation_list2 = [self.adbback_index, self.adbkill_dingding, self.adbpower]
         for operation in operation_list2:
@@ -47,7 +48,6 @@ def with_open_close_dingding(func):
 
 
 class DingDing:
-
     # 初始化，设置adb目录
     def __init__(self, adb_dir):
         self.directory = adb_dir
@@ -77,7 +77,7 @@ class DingDing:
     # 上班(极速打卡)
     @with_open_close_dingding
     def goto_work(self):
-        if is_debug == 1:
+        if is_debug != 1:
             operation_list = [self.adbclick_playcard]
             for operation in operation_list:
                 process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
@@ -90,7 +90,7 @@ class DingDing:
     # 下班
     @with_open_close_dingding
     def after_work(self):
-        if is_debug == 1:
+        if is_debug != 1:
             operation_list = [self.adbclick_playcard]
             for operation in operation_list:
                 process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
@@ -101,54 +101,58 @@ class DingDing:
         print("下班打卡成功")
 
 
-# 随机打卡时间段
-def random_minute():
-    return random.randint(30, 50)
+def get_tomorrow_call_time(hour, minute):
+    tomorrow = int(time.mktime(time.strptime(str(datetime.date.today() + datetime.timedelta(days=1)), '%Y-%m-%d')))
+    return tomorrow + (hour * 3600) + (minute * 60)
 
 
-# 包装循环函数，传入随机打卡时间点
-def incode_loop(func, minute):
-    """
-    包装start_loop任务调度函数，主要是为了传入随机分钟数。保证在不打卡的情况下能保持分钟数不变。
-    :param func: star_loop
-    :param minute: 随机分钟数
-    :return: None
-    """
-    # 判断时间当超过上班时间则打下班卡。否则则打上班卡。
-    if go_hour <= datetime.datetime.now().hour < back_hour:
-        # 用来分类上班和下班。作为参数传入任务调度
-        hourtype = 1
-        print("下次将在", str(back_hour), ":", str(minute), "打卡")
-    else:
-        hourtype = 2
-        print("下次将在", str(go_hour), ":", str(minute), "打卡")
-    # 执行任务调度函数
-    func(hourtype, minute)
+def get_today_call_time(hour, minute):
+    today = int(time.mktime(time.strptime(str(datetime.date.today()), '%Y-%m-%d')))
+    return today + (hour * 3600) + (minute * 60)
+
+
+def call_later_delay(func, hour, minute):
+    return func(hour, minute) - int(datetime.datetime.now().timestamp())
+
+
+def do_goto_work():
+    DingDing(directory).goto_work()
+    setup_after_work()
+
+
+def do_after_work():
+    DingDing(directory).after_work()
+    ioloop.IOLoop.instance().call_later(60 * 60, start_loop)
+
+
+def setup_after_work():
+    random_time = random.randint(5, 30)
+    delay = abs(call_later_delay(get_today_call_time, back_hour, random_time))
+    ioloop.IOLoop.instance().call_later(delay, do_after_work)
+
+
+def setup_goto_work():
+    random_time = random.randint(30, 59)
+    delay = abs(call_later_delay(get_today_call_time, go_hour, random_time))
+    ioloop.IOLoop.instance().call_later(delay, do_goto_work)
 
 
 # 任务调度
-def start_loop(hourtype, minute):
+def start_loop():
     """
-    每次循环完成，携带生成的随机分钟数来再次进行循环，当打卡后，再重新生成随机数
-    :param hourtype: 设置的上班时间点
-    :param minute: 随机生成的分钟数（30-55）
+    判断上下班类型,确定上下班打卡时间
     :return: None
     """
     now_time = datetime.datetime.now()
     now_hour = now_time.hour
-    now_minute = now_time.minute
-    # 上班，不是周末（双休），小时对应，随机分钟对应
-    if hourtype == 2 and now_hour == go_hour and now_minute == minute and is_weekend():
-        random_time = random_minute()
-        DingDing(directory).goto_work(random_time)
-        scheduler.enter(0, 0, incode_loop, (start_loop, random_time,))
-    if hourtype == 1 and now_hour == back_hour and now_minute == minute and is_weekend():
-        random_time = random_minute()
-        DingDing(directory).after_work(random_time)
-        scheduler.enter(0, 0, incode_loop, (start_loop, random_time,))
+
+    # 上班,小时对应，随机分钟对应
+    if now_hour == go_hour:
+        setup_goto_work()
+    if now_hour == back_hour:
+        setup_after_work()
     else:
-        print(now_hour, ':', now_minute)
-        scheduler.enter(60, 0, start_loop, (hourtype, minute,))
+        ioloop.IOLoop.instance().call_later(60, start_loop)
 
 
 # 是否是周末
@@ -158,23 +162,14 @@ def is_weekend():
     """
     now_time = datetime.datetime.now().strftime("%w")
     if now_time == "6" or now_time == "0":
-        return False
-    else:
         return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
-    pass
-    # ======formal
-    # scheduler.enter(0, 0, incode_loop, (start_loop, random_minute(),))
-    # scheduler.run()
-    # ====test
-    # image = SendMessage(2).TailorImage("D:\\screen.png")
-    # shotmessage = SendMessage(2).baidu_img_str(image)
-    # print(shotmessage)
-    # SendMessage(shotmessage)
-    # SendMessage()
-    dingding = DingDing(directory)
-    dingding.goto_work()
-    # ==== weekend
-    print(is_weekend())
+    try:
+        ioloop.IOLoop.instance().call_later(1, start_loop)
+        ioloop.IOLoop.instance().start()
+    except Exception as err:
+        print("err = {}".format(err))

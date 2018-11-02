3f364267533a6f87f508fbf621a04fecc6e106ff
rocknio <rocknio@gmail.com>
2018-11-02 17:46:15 +0800

    添加短信功能

--- twilio_sms/twilio_sms_utils.py
+++ twilio_sms/twilio_sms_utils.py
@@ -6,15 +6,15 @@ __author__ = 'Neo'
 
 
 def send_sms(dest_no, content):
-    account_sid = "AC0116268004c721182cbd674ccda2e332"
-    auth_token = "ec04d5aaa2aaa5b2e9bad54a016911d5"
+    account_sid = "twilio account id"
+    auth_token = "twilio auth token"
 
     try:
         client = Client(account_sid, auth_token)
 
         message = client.messages.create(
             to=dest_no,
-            from_="+13163336152",
+            from_="twilio sms number",
             body=content
         )
 
